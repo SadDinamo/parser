c3830c88e1d182d967ea4b010826f1f6541eae03
SadDinamo <SadDinamo@yandex.ru>
2022-12-07 18:09:47 +0300

    ajax for getting finviz futures and Cnn fear and greed data

--- main/static/js/cnnFearAndGreed.js
+++ main/static/js/cnnFearAndGreed.js
@@ -1,113 +1,7 @@
 // import series from "../highcharts/code/es-modules/Core/Series/Series";
 
-Highcharts.chart('hc-fear-and-greed-container', {
-    chart: {
-        type: 'gauge',
-        plotBackgroundColor: null,
-        plotBackgroundImage: null,
-        plotBorderWidth: 0,
-        plotShadow: false,
-        height: '45%'
-    },
-    title: {
-        text: ''
-    },
-    credits: {
-        enabled: false
-    },
-    tooltip: {
-        enabled: false
-    },
-    navigation: {
-        buttonOptions: {
-            enabled: false
-        }
-    },
-    pane: {
-        startAngle: -90,
-        endAngle: 90,
-        background: null,
-        center: ['50%', '100%'],
-        size: '180%'
-    },
-    // the value axis
-    yAxis: {
-        min: 0,
-        max: 100,
-        tickPixelInterval: 100,
-        tickPosition: 'inside',
-        tickColor: Highcharts.defaultOptions.chart.backgroundColor || '#FFFFFF',
-        tickLength: 30,
-        tickWidth: 2,
-        minorTickInterval: null,
-        labels: {
-            distance: 15,
-            style: {
-                fontSize: '14px'
-            }
-        },
-        plotBands: [{
-            from: 0,
-            to: 25,
-            color: '#F47378',
-            thickness: 40
-        }, {
-            from: 25,
-            to: 45,
-            color: '#FBCFD0',
-            thickness: 40
-        }, {
-            from: 45,
-            to: 55,
-            color: '#EDD3ED',
-            thickness: 40
-        }, {
-            from: 55,
-            to: 75,
-            color: '#C8EFD4',
-            thickness: 40
-        }, {
-            from: 75,
-            to: 100,
-            color: '#6BD089',
-            thickness: 40
-        }]
-    },
-    series: [{
-        name: 'Fear and greed index',
-        data: [0],
-        dataLabels: {
-            enabled: false,
-            // format: '{y}',
-            borderWidth: 0,
-            color: (
-                Highcharts.defaultOptions.title &&
-                Highcharts.defaultOptions.title.style &&
-                Highcharts.defaultOptions.title.style.color
-            ) || '#333333',
-            style: {
-                fontSize: '16px'
-            }
-        },
-        dial: {
-            radius: '80%',
-            backgroundColor: 'gray',
-            baseWidth: 12,
-            baseLength: '0%',
-            rearLength: '0%'
-        },
-        pivot: {
-            backgroundColor: 'gray',
-            radius: 6
-        }
-    }]
-});
-
 function getCnnFearAndGreedData() {
     // Ajax
-    const chart = Highcharts.charts[0];
-    const point = chart.series[0].points[0];
-    let newVal;
     let s = new Date().toLocaleString();
     if (true) {
         $.ajax({
@@ -119,13 +13,41 @@ function getCnnFearAndGreedData() {
             // mode: 'same-origin', // Do not send CSRF token to another domain.
             success: function (data) {
                 if (data) {
-                    newVal = data['fear_and_greed']['score'];
-                    point.update(newVal);
-                    document.getElementById('FnG-heading').innerText = 'CNN Fear and Greed index: ' +
-                        Math.floor(data['fear_and_greed']['score']) + ' ' + data['fear_and_greed']['rating'];
-                    // document.getElementById('yahooFinanceProgressBarLabel').innerText = percent + '% : ' + data['ticker_name'];
-                    // document.getElementById('yahooFinanceProgressBar').setAttribute('aria-valuenow', percent);
-                    // document.getElementById("yahooFinanceProgressBar").style.setProperty('width', percent + '%');
+                    document.getElementById("cnn-fear-and-greed-value").innerText =
+                        '' + Math.round(data['fear_and_greed']['score']);
+                    document.getElementById("cnn-fear-and-greed-change").innerText =
+                        '' + Math.round((data['fear_and_greed']['score'] - data['fear_and_greed']['previous_close'])
+                            * 10) / 100;
+                    document.getElementById("cnn-fear-and-greed-comment").innerText =
+                        '' + data['fear_and_greed']['rating'];
+
+                    // add and show toast notification
+                    let toasts = document.getElementById('toasts-section');
+                    let toastHeader = document.createElement('div');
+                    toastHeader.className = 'toast-header';
+                    let toastHeaderText = document.createElement('strong');
+                    toastHeaderText.className = 'me-auto';
+                    toastHeaderText.innerText = 'Cnn fear and greed';
+                    let toastMessage = document.createElement('div');
+                    toastMessage.className = 'toast-body';
+                    toastMessage.innerText = 'Data updated';
+                    let toastButtonClose = document.createElement('button');
+                    toastButtonClose.type = 'button';
+                    toastButtonClose.className = 'btn-close me-2 m-auto';
+                    toastButtonClose.setAttribute('data-bs-dismiss', 'toast');
+                    toastButtonClose.setAttribute('aria-label', 'Close');
+                    toastHeader.append(toastHeaderText, toastButtonClose);
+                    let toast = document.createElement('div');
+                    toast.className = 'toast hide align-items-center border-0';
+                    toast.setAttribute('role', 'alert');
+                    toast.setAttribute('aria-live', 'assertive');
+                    toast.setAttribute('aria-atomic', 'true');
+                    toast.append(toastHeader);
+                    toast.append(toastMessage);
+                    toasts.append(toast);
+                    let toastBs = new bootstrap.Toast(toast);
+                    toastBs.show();
+                    toastBs = null;
                 } else {
                     console.log(s + ": Unable to get data for get_cnn_fear_and_greed_stats")
                 }
--- main/static/js/finvizFutures.js
+++ main/static/js/finvizFutures.js
@@ -1,8 +1,5 @@
 function getFinvizFuturesData() {
     // Ajax
-    const chart = Highcharts.charts[0];
-    const point = chart.series[0].points[0];
-    let newVal;
     let s = new Date().toLocaleString();
     if (true) {
         $.ajax({
@@ -14,15 +11,68 @@ function getFinvizFuturesData() {
             // mode: 'same-origin', // Do not send CSRF token to another domain.
             success: function (data) {
                 if (data) {
-                    document.getElementById('finvizFuturesGold').innerText = data['GC']['label'] +
-                        " " + data['GC']['last'] + " " + data['GC']['change'] + "%";
-                    document.getElementById('finvizFuturesBrent').innerText = data['QA']['label'] +
-                        " " + data['QA']['last'] + " " + data['QA']['change'] + "%";
-                    document.getElementById('finvizNasdaq100').innerText = data['NQ']['label'] +
-                        " " + data['NQ']['last'] + " " + data['NQ']['change'] + "%";
-                    document.getElementById('finvizNaturalGas').innerText = data['NG']['label'] +
-                        " " + data['NG']['last'] + " " + data['NG']['change'] + "%";
-                    // console.log(data);
+                    document.getElementById('tr-finviz-GC-name').innerText = data['GC']['label'];
+                    document.getElementById('tr-finviz-GC-value').innerText = data['GC']['last'];
+                    document.getElementById('tr-finviz-GC-change').innerText = data['GC']['change'];
+                    document.getElementById('tr-finviz-GC-comment').innerText = '';
+
+                    document.getElementById('tr-finviz-QA-name').innerText = data['QA']['label'];
+                    document.getElementById('tr-finviz-QA-value').innerText = data['QA']['last'];
+                    document.getElementById('tr-finviz-QA-change').innerText = data['QA']['change'];
+                    document.getElementById('tr-finviz-QA-comment').innerText = '';
+
+                    document.getElementById('tr-finviz-NQ-name').innerText = data['NQ']['label'];
+                    document.getElementById('tr-finviz-NQ-value').innerText = data['NQ']['last'];
+                    document.getElementById('tr-finviz-NQ-change').innerText = data['NQ']['change'];
+                    document.getElementById('tr-finviz-NQ-comment').innerText = '';
+
+                    document.getElementById('tr-finviz-NG-name').innerText = data['NG']['label'];
+                    document.getElementById('tr-finviz-NG-value').innerText = data['NG']['last'];
+                    document.getElementById('tr-finviz-NG-change').innerText = data['NG']['change'];
+                    document.getElementById('tr-finviz-NG-comment').innerText = '';
+
+                    document.getElementById('tr-finviz-ZW-name').innerText = data['ZW']['label'];
+                    document.getElementById('tr-finviz-ZW-value').innerText = data['ZW']['last'];
+                    document.getElementById('tr-finviz-ZW-change').innerText = data['ZW']['change'];
+                    document.getElementById('tr-finviz-ZW-comment').innerText = '';
+
+                    document.getElementById('tr-finviz-ES-name').innerText = data['ES']['label'];
+                    document.getElementById('tr-finviz-ES-value').innerText = data['ES']['last'];
+                    document.getElementById('tr-finviz-ES-change').innerText = data['ES']['change'];
+                    document.getElementById('tr-finviz-ES-comment').innerText = '';
+
+                    document.getElementById('tr-finviz-PL-name').innerText = data['PL']['label'];
+                    document.getElementById('tr-finviz-PL-value').innerText = data['PL']['last'];
+                    document.getElementById('tr-finviz-PL-change').innerText = data['PL']['change'];
+                    document.getElementById('tr-finviz-PL-comment').innerText = '';
+
+                    // add and show toast notification
+                    let toasts = document.getElementById('toasts-section');
+                    let toastHeader = document.createElement('div');
+                    toastHeader.className = 'toast-header';
+                    let toastHeaderText = document.createElement('strong');
+                    toastHeaderText.className = 'me-auto';
+                    toastHeaderText.innerText = 'Finviz futures';
+                    let toastMessage = document.createElement('div');
+                    toastMessage.className = 'toast-body';
+                    toastMessage.innerText = 'Data updated';
+                    let toastButtonClose = document.createElement('button');
+                    toastButtonClose.type = 'button';
+                    toastButtonClose.className = 'btn-close me-2 m-auto';
+                    toastButtonClose.setAttribute('data-bs-dismiss', 'toast');
+                    toastButtonClose.setAttribute('aria-label', 'Close');
+                    toastHeader.append(toastHeaderText, toastButtonClose);
+                    let toast = document.createElement('div');
+                    toast.className = 'toast hide align-items-center border-0';
+                    toast.setAttribute('role', 'alert');
+                    toast.setAttribute('aria-live', 'assertive');
+                    toast.setAttribute('aria-atomic', 'true');
+                    toast.append(toastHeader);
+                    toast.append(toastMessage);
+                    toasts.append(toast);
+                    let toastBs = new bootstrap.Toast(toast);
+                    toastBs.show();
+                    toastBs = null;
                 } else {
                     console.log(s + ": Unable to get data for get_finviz_futures_data")
                 }
